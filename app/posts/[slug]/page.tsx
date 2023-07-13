b4195a3c8e721c8c8bb669e93e508fdd50c42502
Sma-source <rabhi.smain@gmail.com>
2023-07-13 17:57:43 +0200

    create function getData

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { client } from "@/lib/sanity";
+
+async function getData(slug: string) {
+  const query = `*[_type == "post" && slug.current == "${slug}"][0]`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
