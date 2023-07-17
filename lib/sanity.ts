99e851770012053c2aa0fa3f861d9984c9de2216
Sma-source <rabhi.smain@gmail.com>
2023-07-17 12:57:48 +0200

    add env

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,12 +1,12 @@
 import { createClient } from "next-sanity";
 
-const projectId = "cuehckq3";
-const dataset = "production";
-const apiVersion = "2023-01-01";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
+  useCdn: false,
 });
